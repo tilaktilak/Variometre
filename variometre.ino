aaeb6df61c149ee2b8fa2ffce8abbec99f91c07e
TILAK Pierre-Louis <tilak@crunchbang>
2015-10-11 17:55:16 +0200

    Mise en place de NilRTOS
    On utilise le code exemple de SdLogging
    On adapte la librairie bmp085 pour utiliser la librairie twi_master
    Préparation de la dérivée de pression

--- variometre.ino
+++ variometre.ino
@@ -1,170 +1,290 @@
+// nilSdLogger.ino is a cleaner version of this
+// example using new Nil RTOS features.
 
-// Data logger based on a FIFO to decouple SD write latency from data
-// acquisition.
-// The FIFO uses two semaphores to synchronize between tasks.
-#include <FreeRTOS_AVR.h>
+/* Data logger based on a FIFO to decouple SD write latency from data
+ * acquisition timing.
+ *
+ * The FIFO uses two semaphores to synchronize between threads.
+ */
+#include <NilRTOS.h>
+// Use NilAnalog to allow the sensor read thread to sleep during ADC conversion.
+#include <NilAnalog.h>
 #include <SdFat.h>
-// interval between points in units of 1024 usec
-const uint16_t intervalTicks = 10;
-//------------------------------------------------------------------------------
-// SD file definitions
+#include <TwiMaster.h>
+// Use tiny unbuffered NilRTOS NilSerial library.
+#include <NilSerial.h>
+
+#include <NilBMP085.h>
+NilBMP085 bmp;
+
+// Macro to redefine Serial as NilSerial to save RAM.
+// Remove definition to use standard Arduino Serial.
+#define Serial NilSerial
+
+// SD chip select pin.
 const uint8_t sdChipSelect = SS;
+
+// Period between points in units of 1024 usec.
+#define PERIOD_TICKS 1000
+
+// Number of ADC chanels to log.
+const uint8_t NUM_ADC = 3;
+
+// Size of fifo in bytes.
+// Adjust so the idle thread has about 100 free bytes.
+const size_t FIFO_SIZE_BYTES = 900;
+//------------------------------------------------------------------------------
+// SD file system.
 SdFat sd;
+
+// Log file.
 SdFile file;
+
+// Maximum overrun count.
+uint16_t maxOverrunCount = 0;
 //------------------------------------------------------------------------------
-// Fifo definitions
-// size of fifo in records
-const size_t FIFO_SIZE = 20;
-// count of data records in fifo
-SemaphoreHandle_t fifoData;
-// count of free buffers in fifo
-SemaphoreHandle_t fifoSpace;
-// data type for fifo item
+// Fifo definitions.
+
+#if PERIOD_TICKS < 50
+typedef uint16_t period_t;
+#else  // PERIOD_TICKS
+typedef uint32_t period_t;
+#endif  // PERIOD_TICKS
+
+// Type for data record.
 struct FifoItem_t {
-    uint32_t usec;
-    int lat;
-    int lon;
-    int alt_gps;
-    int alt_bmp;
-    int error;
+    period_t usec;   // Point start time in usec.
+
+    uint16_t value[NUM_ADC];  // ADC values.
+
+    uint16_t overruns;  // Count of data overruns since last point.
 };
-// array of data items
+
+const uint16_t FIFO_SIZE = FIFO_SIZE_BYTES/sizeof(FifoItem_t);
+
+// Array of fifo data records.
 FifoItem_t fifoArray[FIFO_SIZE];
+
+// Count of data records in the fifo.
+SEMAPHORE_DECL(fifoData, 0);
+
+// Count of free records in the fifo.
+SEMAPHORE_DECL(fifoSpace, FIFO_SIZE);
 //------------------------------------------------------------------------------
-// handle for sensor task
-TaskHandle_t sens;
-static void Task1(void *arg) {
-    // index of record to be filled
+// Sensor read thread.
+
+// Declare a stack with 16 bytes beyond context switch and interrupt needs.
+NIL_WORKING_AREA(waThread1, 64);
+
+// Declare thread function for thread 1.
+NIL_THREAD(Thread1, arg) {
+
+    static period_t     old_time;
+    static uint16_t     old_pres
+
+    // Index of record to be filled.
     size_t fifoHead = 0;
-    // count of overrun errors
-    int error = 0;
-    // dummy data
-    int count = 0;
-    // initialise the ticks variable with the current time.
-    TickType_t ticks = xTaskGetTickCount();
+
+    // Count of overrun errors.
+    int overrunCount = 0;
+
+    // Time for next read.
+    systime_t wakeTime = nilTimeNow();
 
     while (1) {
-        // wait until time for next data point
-        vTaskDelayUntil(&ticks, intervalTicks);
-        // get a buffer
-        if (xSemaphoreTake(fifoSpace, 0) != pdTRUE) {
-            // fifo full - indicate missed point
-            error++;
+        wakeTime += PERIOD_TICKS;
+
+        // Sleep until time for next data point
+        nilThdSleepUntil(wakeTime);
+
+        // Get an empty record.
+        if (nilSemWaitTimeout(&fifoSpace, TIME_IMMEDIATE) != NIL_MSG_OK) {
+            // Don't count overrun if at end of run.
+            if (Serial.available()) continue;
+
+            // Fifo full, count missed point.
+            if (++overrunCount > maxOverrunCount) {
+                maxOverrunCount = overrunCount;
+            }
             continue;
         }
+        // Pointer to empty record.
         FifoItem_t* p = &fifoArray[fifoHead];
+
+        // Start time in usec.
         p->usec = micros();
-        // replace next line with data read from sensor such as
-        // p->lat = analogRead(0);
-        p->lat = count++;
-        p->lon = count++;
-        p->alt_gps = count++;
-        p->error = error;
-        error = 0;
-        // signal new data
-        xSemaphoreGive(fifoData);
-        // advance FIFO index
+
+        // Read ADC data.
+        //for (int i = 0; i < NUM_ADC; i++) {
+            // nilAnalogRead() sleeps during ADC conversion.
+            p->value[0] = bmp.readTemperature();
+            p->value[1] = bmp.readAltitude();
+            new_pres = bmp.readPressure();
+            p->value[3] = (new_pres-old_pres)/(p->usec - old_time); // Pressure derivative
+            old_pres = new_pres;
+            old_time = p->usec;
+        //}
+        //p->value[0] = bmp.readPressure();
+        p->overruns = overrunCount;
+        overrunCount = 0;
+
+        // Signal new data for SD write thread.
+        nilSemSignal(&fifoData);
+
+        // Advance FIFO index.
         fifoHead = fifoHead < (FIFO_SIZE - 1) ? fifoHead + 1 : 0;
     }
 }
 //------------------------------------------------------------------------------
-// SD write task
-static void Task2(void *arg) {
-    // FIFO index for record to be written
-    size_t fifoTail = 0;
-    // time in micros of last point
-    uint32_t last = 0;
-    char value[6];
-    while(1) {
-        // wait for next data record
-        xSemaphoreTake(fifoData, portMAX_DELAY);
-        FifoItem_t* p = &fifoArray[fifoTail];
-        Serial.println("B");
-        Serial.print(p->usec);
-        sprintf(value,"%06d",p->lat);
-        Serial.print(value);
-        sprintf(value,"%06d",p->lon);
-        Serial.print("N");
-        Serial.print(value);
-        Serial.print("W");
-        Serial.print("A");
-        sprintf(value,"%05d",p->alt_gps);
-        Serial.print(value);
-        sprintf(value,"%05d",p->alt_bmp);
-        Serial.print(value);
-#if 0
-        // print interval between points
-        if (last) {
-            file.print(p->usec - last);
-        } else {
-            file.write("NA");
-        }
-        last = p->usec;
-        file.write(',');
-        file.print(p->lat);
-        file.write(',');
-        file.println(p->error);
-        // release record
-#endif
-        xSemaphoreGive(fifoSpace);
-        // advance FIFO index
-        fifoTail = fifoTail < (FIFO_SIZE - 1) ? fifoTail + 1 : 0;
-        // check for end run
-        if (Serial.available()) {
-#if 0      // close file to insure data is saved correctly
-            file.close();
-#endif
-            // print messages
-            Serial.println(F("Done"));
-            Serial.print(F("Task1 unused stack entries: "));
-            Serial.println(uxTaskGetStackHighWaterMark(sens));
-            Serial.print(F("Task2 unused stack entries: "));
-            Serial.println(uxTaskGetStackHighWaterMark(0));
-            Serial.print(F("Free heap (bytes): "));
-            Serial.println(freeHeap());
-            while(1);
-        }
-    }
-}
-//------------------------------------------------------------------------------
-void setup() {
+/*
+ * Threads static table, one entry per thread.  A thread's priority is
+ * determined by its position in the table with highest priority first.
+ *
+ * These threads start with a null argument.  A thread's name is also
+ * null to save RAM since the name is currently not used.
+ */
+    NIL_THREADS_TABLE_BEGIN()
+    NIL_THREADS_TABLE_ENTRY(NULL, Thread1, NULL, waThread1, sizeof(waThread1))
+NIL_THREADS_TABLE_END()
+    //------------------------------------------------------------------------------
+    void setup() {
 
-    // task creation status
-    portBASE_TYPE s1, s2;
-    Serial.begin(9600);
-    Serial.println(F("Type any character to begin"));
-    while(!Serial.available());
-    // open file
+        Serial.begin(9600);
+        if (!bmp.begin()) {
+            Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+            while (1) {}
+        }
+        Serial.println(F("type any character to begin"));
+        while (Serial.read() < 0);
+
+        // Can read maximum of six ADC values per tick.
+        if (6*PERIOD_TICKS < NUM_ADC) {
+            Serial.println(F("Halting, too many ADC reads per tick."));
+            while (1) {}
+        }
+
+        // Initialize SD and create or open and truncate data file.
 #if 0
-    if (!sd.begin(sdChipSelect)
-            || !file.open("DATA.CSV", O_CREAT | O_WRITE | O_TRUNC)) {
-        Serial.println(F("SD problem"));
-        sd.errorHalt();
-    }
+        if (!sd.begin(sdChipSelect)
+                || !file.open("DATA.CSV", O_CREAT | O_WRITE | O_TRUNC)) {
+            Serial.println(F("SD problem"));
+            sd.errorHalt();
+        }
+        file.print(F("Period,"));
+        for (int i = 0;i < NUM_ADC; i++) {
+            file.print(F("ADC"));
+            file.print(i);
+            file.print(',');
+        }
+        file.println(F("overruns"));
 #endif
-    // initialize fifoData semaphore to no data available
-    fifoData = xSemaphoreCreateCounting(FIFO_SIZE, 0);
-    // initialize fifoSpace semaphore to FIFO_SIZE free records
-    fifoSpace = xSemaphoreCreateCounting(FIFO_SIZE, FIFO_SIZE);
-    // create sensor task at priority two
-    s1 = xTaskCreate(Task1, NULL, configMINIMAL_STACK_SIZE, NULL, 2, &sens);
-    // create SD write task at priority one
-    s2 = xTaskCreate(Task2, NULL, configMINIMAL_STACK_SIZE + 200, NULL, 1, NULL);
-    // check for creation errors
-    if (fifoData == NULL || fifoSpace == NULL || s1 != pdPASS || s2 != pdPASS ) {
-        Serial.println(F("Creation problem"));
+        // Throw away serial input.
+        while (Serial.read() >= 0) {
+            delay(10);
+        }
+        Serial.println(F("type any character to end"));
+
+        // Start kernel.
+        nilSysBegin();
+    }
+//------------------------------------------------------------------------------
+// Write SD in loop.
+//
+// Loop is the idle thread.  The idle thread must not invoke any
+// kernel primitive able to change its state to not runnable.
+
+// FIFO index for record to be written.
+size_t fifoTail = 0;
+
+// Time in micros for last point.
+period_t last = 0;
+
+// Maximum SD write latency for a record.
+uint32_t maxLatency = 0;
+
+// Minimum free record count.
+cnt_t minFreeCount = FIFO_SIZE;
+
+// Don't write interval for first point.
+bool first = true;
+
+void loop() {
+
+    // Check for end run.
+    if (Serial.available()) {
+
+        // Close file, print memory stats, possible error message, and stop.
+#if 0
+        file.close();
+#endif
+        Serial.println(F("Done"));
+        Serial.print(F("FIFO record count: "));
+        Serial.println(FIFO_SIZE);
+        Serial.print(F("Minimum free record count: "));
+        Serial.println(minFreeCount);
+        Serial.print(F("Maximum SD write latency: "));
+        Serial.print(maxLatency);
+        Serial.println(F(" usec"));
+        nilPrintUnusedStack(&Serial);
+        if (maxOverrunCount) {
+            Serial.println();
+            Serial.println(F("** overrun errors **"));
+            Serial.print(F("Maximum overrun count: "));
+            Serial.println(maxOverrunCount);
+        }
+        // Done!
         while(1);
     }
-    // throw away serial input
-    while (Serial.read() >= 0);
-    Serial.println(F("Type any character to end"));
-    // start scheduler
-    vTaskStartScheduler();
-    Serial.println(F("Insufficient RAM"));
-    while(1);
-}
-//------------------------------------------------------------------------------
-void loop() {
-    // Not used - idle loop has a very small, configMINIMAL_STACK_SIZE, stack
-    // loop must never block
+
+    // Try to access yhe next data point. The idle thread must not block.
+    // Use TIME_IMMEDIATE so nilWaitSemTimeout will not block.
+    if (nilSemWaitTimeout(&fifoData, TIME_IMMEDIATE ) != NIL_MSG_OK) {
+        return;
+    }
+
+    FifoItem_t* p = &fifoArray[fifoTail];
+
+    // Measure write latency for record.
+    uint32_t latency = micros();
+    if (first){
+        first = false;
+    }
+    Serial.print("Temp:");
+    Serial.print(p->value[0]);
+    Serial.print("\r\n");
+    Serial.println("Altitude:");
+    Serial.print(p->value[1]);
+    Serial.print("\r\n");
+#if 0
+    // Print interval between points.
+    if (first) {
+        first = false;
+        file.write("NA,");
+    }
+    else {
+        file.printField(p->usec - last, ',');
+    }
+    // Remember time of last data point.
+    last = p->usec;
+
+    // Print ADC values.
+    for (int i = 0; i < NUM_ADC; i++) {
+        file.printField(p->value[i],',');
+    }
+    file.printField(p->overruns, '\n');
+#endif
+
+    // Check latency.
+    latency = micros() - latency;
+    if (latency > maxLatency) maxLatency = latency;
+
+    // Check number of free records
+    cnt_t cnt = nilSemGetCounter(&fifoSpace);
+    if (cnt < minFreeCount) minFreeCount = cnt;
+
+    // Release free data record.
+    nilSemSignal(&fifoSpace);
+
+    // Advance FIFO index.
+    fifoTail = fifoTail < (FIFO_SIZE - 1) ? fifoTail + 1 : 0;
 }
